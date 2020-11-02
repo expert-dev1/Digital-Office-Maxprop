db96908228c985d80edbe1e009081fc0272ccf59
Scott Joscelyne <oasis@maxprop.digital>
2020-11-02 10:07:34 +0200

    Initial Publish

---
+++ src/app/commercial/commercial-index/commercial-index.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+import { FormioResourceIndexComponent } from 'angular-formio/resource';
+
+@Component({
+  selector: 'app-commercial-index',
+  templateUrl: './commercial-index.component.html',
+  styleUrls: ['./commercial-index.component.scss']
+})
+export class CommercialIndexComponent extends FormioResourceIndexComponent { }
---
+++ src/app/residential/residential-index/residential-index.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+import { FormioResourceIndexComponent } from 'angular-formio/resource';
+@Component({
+  selector: 'app-residential-index',
+  templateUrl: './residential-index.component.html',
+  styleUrls: ['./residential-index.component.scss']
+})
+export class ResidentialIndexComponent extends FormioResourceIndexComponent { }
